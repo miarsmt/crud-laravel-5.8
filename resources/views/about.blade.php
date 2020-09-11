49b74a851a7f5a407a545eae1de54f9e63b3431f
miarsmt <rusmiatim973@gmail.com>
2020-09-11 23:47:11 +0700

    memanggil parameter yg di lempar

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -6,7 +6,7 @@
 <div class="container">
     <div class="row">
         <div class="col">
-            <h1 class="mt-3">Hello, anya!</h1>
+            <h1 class="mt-3">Hello, {{ $nama }}!</h1>
 
         </div>
     </div>
