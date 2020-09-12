8edd8de978d1d448b4259691ee6498a85462829a
miarsmt <rusmiatim973@gmail.com>
2020-09-12 09:08:17 +0700

    membuat model untuk mencoba teknik ORM Eloquent

---
+++ app/Student.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Student extends Model
+{
+    // tidak perlu ditambahkan apapun karena tabel nya sudah jamak sesuai dengan nama model nya
+}
