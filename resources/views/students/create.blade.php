534f1a960081e034a7cfcbf4e32840dbf27d3f67
miarsmt <rusmiatim973@gmail.com>
2020-09-12 19:41:23 +0700

    menambahkan helper old input

--- resources/views/students/create.blade.php
+++ resources/views/students/create.blade.php
@@ -12,25 +12,25 @@
                 @csrf
                 <div class="form-group">
                     <label for="nama">Nama</label>
-                    <input type="text" name="nama" class="form-control @error('nama') is-invalid @enderror" id="nama" placeholder="Masukkan Nama">
+                    <input type="text" name="nama" class="form-control @error('nama') is-invalid @enderror" id="nama" placeholder="Masukkan Nama" value="{{ old('nama') }}">
                     @error('nama')<div class="invalid-feedback">
                         {{ $message }}
                     </div>@enderror
                 </div>
                 <div class="form-group">
                     <label for="nrp">NRP</label>
-                    <input type="text" name="nrp" class="form-control @error('nrp') is-invalid @enderror" id="nrp" placeholder="Masukkan NRP">
+                    <input type="text" name="nrp" class="form-control @error('nrp') is-invalid @enderror" id="nrp" placeholder="Masukkan NRP" value="{{ old('nrp') }}">
                     @error('nrp')<div class="invalid-feedback">
                         {{ $message }}
                     </div>@enderror
                 </div>
                 <div class="form-group">
                     <label for="email">Email</label>
-                    <input type="text" name="email" class="form-control" id="email" placeholder="Masukkan Email">
+                    <input type="text" name="email" class="form-control" id="email" placeholder="Masukkan Email" value="{{ old('email') }}">
                 </div>
                 <div class="form-group">
                     <label for="jurusan">Jurusan</label>
-                    <input type="text" name="jurusan" class="form-control" id="jurusan" placeholder="Masukkan Jurusan">
+                    <input type="text" name="jurusan" class="form-control" id="jurusan" placeholder="Masukkan Jurusan" value="{{ old('jurusan') }}">
                 </div>
                 <button type="submit" class="btn btn-primary">Tambah Data</button>
             </form>
