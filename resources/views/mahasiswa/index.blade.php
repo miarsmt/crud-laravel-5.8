741f55b82efa1a3c5df090f5ebcea12200900879
miarsmt <rusmiatim973@gmail.com>
2020-09-12 08:18:11 +0700

    membuat folder mahasiswa dan view index.blade.php

---
+++ resources/views/mahasiswa/index.blade.php
@@ -0,0 +1,14 @@
+@extends('layout.main')
+
+@section('title', 'Daftar Mahasiswa')
+
+@section('container')
+<div class="container">
+    <div class="row">
+        <div class="col">
+            <h1 class="mt-3">Daftar Mahasiswa</h1>
+
+        </div>
+    </div>
+</div>
+@endsection
