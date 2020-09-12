492a7f4552e422e16ce542b48b11f6f8606f5b26
miarsmt <rusmiatim973@gmail.com>
2020-09-12 08:23:21 +0700

    memodifikasi view membuat tabel di view isi nya statis

--- resources/views/mahasiswa/index.blade.php
+++ resources/views/mahasiswa/index.blade.php
@@ -8,6 +8,31 @@
         <div class="col">
             <h1 class="mt-3">Daftar Mahasiswa</h1>
 
+            <table class="table">
+                <thead class="thead-dark">
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">Nama</th>
+                        <th scope="col">NRP</th>
+                        <th scope="col">Email</th>
+                        <th scope="col">Jurusan</th>
+                        <th scope="col">Aksi</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <th scope="row">1</th>
+                        <td>Anya Geraldine</td>
+                        <td>040305067</td>
+                        <td>anyageraldine@gmail.com</td>
+                        <td>Desain Komuniksi Visual</td>
+                        <td>
+                            <a href="" class="badge badge-success">Edit</a>
+                            <a href="" class="badge badge-danger">Delete</a>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
         </div>
     </div>
 </div>
